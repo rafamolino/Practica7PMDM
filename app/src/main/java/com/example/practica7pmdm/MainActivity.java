a1a4e4d0a54afc8738bc36e0147bbbed466de77e
rafamolino <molinorafael99@gmail.com>
2019-03-20 14:57:46 +0100

    Funcionamiento imgMapa close#3

--- app/src/main/java/com/example/practica7pmdm/MainActivity.java
+++ app/src/main/java/com/example/practica7pmdm/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView imgMapa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imgMapa = findViewById(R.id.imgMapa);

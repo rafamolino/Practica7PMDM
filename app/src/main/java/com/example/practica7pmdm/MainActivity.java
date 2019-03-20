dc572df23b318a676c182e19376759f2bc4a0831
rafamolino <molinorafael99@gmail.com>
2019-03-20 14:55:56 +0100

    Funcionamiento imgMapa close#11  close#7

--- app/src/main/java/com/example/practica7pmdm/MainActivity.java
+++ app/src/main/java/com/example/practica7pmdm/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.practica7pmdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner categorias;
+    ImageView imgMapa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgMapa = findViewById(R.id.imgMapa);
+
 
         final String[] categoriaLugar = {"Restaurantes" , "Cines", "Tiendas" , "Parques", "PUBS"};
         ArrayAdapter adaptadorCategoriaLugar = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, categoriaLugar);
 
         final Spinner OpcionesEC = findViewById(R.id.categorias);
         OpcionesEC.setAdapter(adaptadorCategoriaLugar);
+        imgMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+            }
+        });
     }
 }
